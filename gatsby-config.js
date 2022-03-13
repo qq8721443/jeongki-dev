c766b4af1b7a0ed9243f88ad5fbf3f4e453c3425
qq8721443 <qq8721443@naver.com>
2022-03-13 15:26:07 +0900

    change loading bar color

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,40 +2,40 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://jeongki.dev",
     title: "Jeongki Dev",
-    description:"Jeongki의 개발 실력 향상을 위한 블로그입니다."
+    description: "Jeongki의 개발 실력 향상을 위한 블로그입니다.",
   },
   plugins: [
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name:'contents',
-        path:`${__dirname}/src/contents`
+        name: "contents",
+        path: `${__dirname}/src/contents`,
       },
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: 'images',
-        path: `${__dirname}/src/images`
-      }
+        name: "images",
+        path: `${__dirname}/src/images`,
+      },
     },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name:"Jeongki Dev",
-        short_name:"JeongkiDev",
-        start_url:'/',
-        display:'standalone',
-        icon: "src/images/favicon-16x16.png"
-      }
+        name: "Jeongki Dev",
+        short_name: "JeongkiDev",
+        start_url: "/",
+        display: "standalone",
+        icon: "src/images/favicon-16x16.png",
+      },
     },
     "gatsby-plugin-mdx",
     {
-      resolve:"gatsby-plugin-nprogress",
-      options:{
-        color: `mediumaquamarine`,
+      resolve: "gatsby-plugin-nprogress",
+      options: {
+        color: `#189AD3`,
         showSpinner: false,
-      }
+      },
     },
     "gatsby-remark-reading-time",
     "gatsby-plugin-image",
@@ -43,6 +43,6 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-react-helmet",
-    "prism-react-renderer"
+    "prism-react-renderer",
   ],
 };
