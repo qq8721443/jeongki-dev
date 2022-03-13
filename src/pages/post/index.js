a5b8171749bb50ca9093de012cb91fb55876cb05
qq8721443 <qq8721443@naver.com>
2022-03-13 15:28:49 +0900

    change post searching bar design

--- src/pages/post/index.js
+++ src/pages/post/index.js
@@ -1,51 +1,62 @@
-import React from 'react';
-import Layout from '../../components/layout';
-import {graphql, useStaticQuery} from 'gatsby';
-import {postContainer, topContainer, topLeft, topRight} from './index.module.css';
-import Postcard from '../../components/postcard';
+import React from "react";
+import Layout from "../../components/layout";
+import { graphql, useStaticQuery } from "gatsby";
+import {
+  postContainer,
+  topContainer,
+  topLeft,
+  topRight,
+  input,
+} from "./index.module.css";
+import Postcard from "../../components/postcard";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export default function Page1() {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
-        allMdx(sort: {fields: frontmatter___date, order:DESC}) {
-          nodes {
-            frontmatter {
-              title
-              date(formatString: "YYYY-MM-DD")
-              tag
-              description
-              hero_image_url {
-                  childImageSharp {
-                      gatsbyImageData(
-                          width:300
-                      )
-                  }
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          frontmatter {
+            title
+            date(formatString: "YYYY-MM-DD")
+            tag
+            description
+            hero_image_url {
+              childImageSharp {
+                gatsbyImageData(width: 300)
               }
             }
-            id
-            fields {
-                slug
-            }
+          }
+          id
+          fields {
+            slug
           }
         }
       }
-    `)
-    return (
-        <Layout>
-            <div className={topContainer}>
-                <div className={topLeft}>
-                    글 목록
-                </div>
-                <div className={topRight}>
-                    <input style={{height:'100%', outline:'none', border:'1px solid #808080', borderRadius:'5px', boxSizing:'border-box', paddingLeft:'10px', marginRight:'10px'}} type="text" placeholder="검색"/>
-                    <input style={{width:'40px', height:'100%', border:'none', cursor:'pointer', backgroundColor:'var(--button-color)', borderRadius:'5px', color:'#fff', textAlign:'center'}} type="button" value="🔍" />
-                </div>
-            </div>
-            <div className={postContainer}>
-            {data.allMdx.nodes.map(item => (
-              <Postcard key={item.id} title={item.frontmatter.title} description={item.frontmatter.description} date={item.frontmatter.date} tag={item.frontmatter.tag} slug={item.fields.slug} thumbnail={item.frontmatter.hero_image_url}/> // title, description, date, tag 필요
-            ))}
-            </div>
-        </Layout>
-    )
+    }
+  `);
+  return (
+    <Layout>
+      <div className={topContainer}>
+        <div className={topLeft}>글 목록</div>
+        <div className={topRight}>
+          <AiOutlineSearch size={25} />
+          <input className={input} type="text" placeholder="검색" />
+        </div>
+      </div>
+      <div className={postContainer}>
+        {data.allMdx.nodes.map((item) => (
+          <Postcard
+            key={item.id}
+            title={item.frontmatter.title}
+            description={item.frontmatter.description}
+            date={item.frontmatter.date}
+            tag={item.frontmatter.tag}
+            slug={item.fields.slug}
+            thumbnail={item.frontmatter.hero_image_url}
+          /> // title, description, date, tag 필요
+        ))}
+      </div>
+    </Layout>
+  );
 }
