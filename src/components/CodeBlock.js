258dbb74dddc207b71d77a204dd103ee7c398072
qq8721443 <qq8721443@naver.com>
2022-03-13 15:27:24 +0900

    change codeblock design

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
-import vsDark from "prism-react-renderer/themes/vsDark"
+import vsDark from "prism-react-renderer/themes/vsDark";
 
 const CodeBlock = ({ children, className }) => {
   const language = className ? className.replace(/language-/, "") : "";
@@ -13,14 +13,28 @@ const CodeBlock = ({ children, className }) => {
       theme={vsDark}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: "20px", fontSize:'1.1rem' }}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
+        <pre
+          className={className}
+          style={{
+            ...style,
+            padding: "20px",
+            fontSize: "1.1rem",
+            borderRadius: "10px",
+            margin: "10px 0px",
+            overflowX: "auto",
+          }}
+        >
+          {tokens.map((line, i) =>
+            i !== tokens.length - 1 ? (
+              <div key={i} {...getLineProps({ line, key: i })}>
+                {i + 1}
+                {"  "}
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ) : null
+          )}
         </pre>
       )}
     </Highlight>
--- src/components/markdown-react-component.js
+++ src/components/markdown-react-component.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React from "react";
 export function Blockquote({ children }) {
-    return (
-        <blockquote style={{paddingLeft:'15px', borderLeft:'7px solid #808080'}}>
-            {children}
-        </blockquote>
-    )
+  return (
+    <blockquote
+      style={{
+        padding: "15px",
+        margin: "15px",
+        borderLeft: "7px solid #808080",
+      }}
+    >
+      {children}
+    </blockquote>
+  );
 }
 
 export function A(props) {
-    return (
-        <a {...props} style={{fontWeight:'500', color:'mediumaquamarine'}}>
-            {props.children}
-        </a>
-    )
+  return (
+    <a {...props} style={{ fontWeight: "500", color: "#189AD3" }}>
+      {props.children}
+    </a>
+  );
+}
+
+export function H2(props) {
+  return (
+    <h2 {...props} style={{ fontWeight: "500", margin: "20px 0px" }}>
+      {props.children}
+    </h2>
+  );
+}
+
+export function H3(props) {
+  return (
+    <h3 {...props} style={{ fontWeight: "400", margin: "20px 0" }}>
+      {props.children}
+    </h3>
+  );
+}
+
+export function Ol(props) {
+  return (
+    <ol {...props} style={{ margin: "0px 20px", padding: "10px" }}>
+      {props.children}
+    </ol>
+  );
+}
+
+export function Ul(props) {
+  return (
+    <ul {...props} style={{ margin: "0px 20px", padding: "10px" }}>
+      {props.children}
+    </ul>
+  );
 }
