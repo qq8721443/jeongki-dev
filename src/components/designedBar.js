b1f7cf61c225e883c57e358ceccfcfbdfbc6088a
qq8721443 <qq8721443@naver.com>
2022-06-26 03:29:57 +0900

    chore(designedBar.js): 불필요 파일 삭제

--- src/components/designedBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function DesignedBar({width, fraction, color}) {
-    return (
-        <div style={{position:'relative', width:width, height:'20px', borderRadius:'15px', backgroundColor:'gray', overflow:'hidden'}}>
-            <div style={{left:0, width:`${fraction}%`, height:'100%', backgroundColor:color}}></div>
-        </div>
-    )
-}
